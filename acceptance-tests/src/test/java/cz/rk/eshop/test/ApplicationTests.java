0c14f638f5e2e64f2c06c5ffbcbcc82a6dc4d4ef
radekcz <radek.krizka@gmail.com>
2021-03-10 16:11:12 +0100

    add acceptance tests

--- acceptance-tests/src/test/java/cz/rk/eshop/test/ApplicationTests.java
+++ acceptance-tests/src/test/java/cz/rk/eshop/test/ApplicationTests.java
@@ -1,23 +1,68 @@
 package cz.rk.eshop.test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.AfterTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.web.client.RestTemplate;
 
 
-@SpringBootTest(classes = ApplicationTests.class)
-@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private static final String ENDPOINT = "http://localhost:8080/watches";
+
+    private RestTemplate restTemplate;
+    private ObjectMapper mapper;
+
+    @BeforeAll
+    public void setup() {
+        restTemplate = new RestTemplate();
+        mapper = new ObjectMapper();
+    }
 
     @Test
-    public void shouldCreateNewWatch() {
-        Assertions.assertTrue(true);
+    public void shouldCreateNewWatch() throws Exception {
+        // create JSON input
+        String jsonPayload = createJSONInput();
+
+        ResponseEntity<String> response = restTemplate.postForEntity(ENDPOINT, jsonPayload, String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode name = root.path("title");
+        Assertions.assertNotNull(name.asText());
+    }
+
+    @Test
+    public void shouldReadWatch() throws Exception {
+        // TODO: first create new watch to test
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(ENDPOINT + "/1", String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode name = root.path("title");
+        Assertions.assertNotNull(name.asText());
+    }
+
+    private String createJSONInput() {
+        // create a JSON object
+        ObjectNode node = mapper.createObjectNode();
+        node.put("title", "Casio");
+        node.put("price", 120000);
+        node.put("description", "Great watch!");
+        return node.toString();
     }
 
 }
